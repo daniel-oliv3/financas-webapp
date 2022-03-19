b3ce6d98ba3aa12e5553c5c9933dac7e2762061d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-19 11:17:08 -0300

    Up

---
+++ pages/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { App } from "../src/pages/App";
+
+const index = () => {
+    return App
+};
+
+export default index;
---
+++ src/pages/App/index.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+    return (
+        <div>
+            Sapup3
+        </div>
+    );
+}
