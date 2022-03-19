fd8108d2a4cd4447696ff404136517e7f1f6fd15
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-19 10:39:44 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
+  color: ${({ theme }) => theme.colors};
+`;
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>Olá, Mundo!</Title>
 }
+
